3218ccf9c56ffb411dbfbd149e1de7c7960cf24c
nazonazopp <61644930+nazonazopp@users.noreply.github.com>
2021-07-01 23:29:43 +0900

    テストコード作成

--- spec/controllers/posts_spec.rb
+++ spec/controllers/posts_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :controller do
-  it "投稿できること" 
+  it "投稿できること" do
+  post = Post.new(
+    user_id:"1"
+  
+ 
+
+  )
+  expect(post.user_id).to eq "1"
+end
 end
--- spec/controllers/users_spec.rb
+++ spec/controllers/users_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe User, type: :controller do
   it "名前が登録できること" 
+  
 end
